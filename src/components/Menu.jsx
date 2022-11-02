66a6ee3af59c6924e76106258ff9e7b8c565a7a9
Timothy-Achonu <achonutimothy@gmail.com>
2022-11-02 11:55:38 +0100

    making the menu in mobile view functional

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import { NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-
-export default function Menu({showMenu, setShowMenu}) {
+export default function Menu({ showMenu, setShowMenu }) {
   return (
-    <div className={`menu ${showMenu ? "show": ""}`}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/counter">Counter</NavLink>
-      <NavLink to="/testErrorBoundary">Test Error boundary</NavLink>
+    <div className={`menu ${showMenu ? "show" : ""}`}>
+      <div
+        className="close"
+        onClick={() => {
+          setShowMenu(false);
+        }}
+      >
+        x
+      </div>
+      <NavLink
+        to="/"
+        onClick={() => {
+          setShowMenu(false);
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/counter"
+        onClick={() => {
+          setShowMenu(false);
+        }}
+      >
+        Counter
+      </NavLink>
+      <NavLink
+        to="/testErrorBoundary"
+        onClick={() => {
+          setShowMenu(false);
+        }}
+      >
+        Test Error boundary
+      </NavLink>
     </div>
-  )
+  );
 }
