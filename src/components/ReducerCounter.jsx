07ba7ecaa065bb47c1c8dc5bb0fb2d4e29e919c7
Timothy-Achonu <achonutimothy@gmail.com>
2022-11-02 12:04:20 +0100

    adding a scroll event to the window object

--- src/components/ReducerCounter.jsx
+++ src/components/ReducerCounter.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
-import useReducerCounter from './CustomHooks/useReducerCounter';
-
+import React, {useEffect} from "react";
+import useReducerCounter from "./CustomHooks/useReducerCounter";
 
 export default function ReducerCounter() {
-    const {count, dispatch, ACTIONS, inputHandler} = useReducerCounter(33);
+  useEffect(() => {
+    window.scrollTo({ behavior: "smooth", top: "0" });
+  }, []);
+  const { count, dispatch, ACTIONS, inputHandler } = useReducerCounter(33);
   return (
-    <div className='counter'>
+    <div className="counter">
       <h1>UseReducer Counter</h1>
-      <div >
-        <input type="text" placeholder='set counter value' 
-        onChange={inputHandler} onBlur={(e) => {e.target.value = ""}}/>
-        <h2 className='count-num'>Count : {count}</h2>
-        <div className='buttons-wrapper'>
-        <button onClick={() => {
-          dispatch({type : ACTIONS.INCREASE})
-        }}>INCREMENT</button>
-        <button onClick={() => {
-          dispatch({type : ACTIONS.DECREASE})
-        }}>DECREMENT</button>
-        <button onClick={() => {
-          dispatch({type : ACTIONS.RESET})
-        }}>RESET</button>
+      <div>
+        <input
+          type="text"
+          placeholder="set counter value"
+          onChange={inputHandler}
+          onBlur={(e) => {
+            e.target.value = "";
+          }}
+        />
+        <h2 className="count-num">Count : {count}</h2>
+        <div className="buttons-wrapper">
+          <button
+            onClick={() => {
+              dispatch({ type: ACTIONS.INCREASE });
+            }}
+          >
+            INCREMENT
+          </button>
+          <button
+            onClick={() => {
+              dispatch({ type: ACTIONS.DECREASE });
+            }}
+          >
+            DECREMENT
+          </button>
+          <button
+            onClick={() => {
+              dispatch({ type: ACTIONS.RESET });
+            }}
+          >
+            RESET
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
